a7eeba641cc92ecd2627230ce5b16fcc9747cd8f
jColaco <me@jColaco.dev>
2022-11-21 22:46:58 +0000

    Studio Begin

---
+++ webApp/my-app/src/Pages/Studio/Studio.jsx
@@ -0,0 +1,17 @@
+import "./Studio.scss"
+
+const Studio = () => {
+    return (
+        <div className="Studio">
+            <div className="pngLights"></div> 
+            <div className="bg">
+                <section>
+                
+                </section>    
+            </div>  
+            <div className="floor"></div>  
+        </div>
+    );
+};
+
+export default Studio;
